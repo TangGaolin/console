ff10b656c7a103ce9b71c4b333885e7570e43e4f
tanggaolin <tanggaolin123@163.com>
2017-09-18 20:04:11 +0800

    update:rbac_node config

--- config/rbac_node.php
+++ config/rbac_node.php
@@ -35,7 +35,7 @@ return [
                 [
                     'node_id'   => '5',
                     'node_name' => 'employee',
-                    'title'     => '员工管理',
+                    'title'     => '职工信息',
                     'child'    => [
                         [
                             'node_id'   => '6',
@@ -56,26 +56,26 @@ return [
                             'resource'  => ['/employee/importEmployee']
                         ],
                         [
-                            'node_id'   => '12',
+                            'node_id'   => '9',
                             'node_name' => 'getEmployeeInfo',
                             'title'     => '员工数据信息',
-                            'resource'  => ['/employee/getEmployeeInfo']
-                        ],
+                            'resource'  => ['/employee/getEmployeeInfo', '/employee/getEmpDataView', '/employee/getEmpOrderList']
+                        ]
                     ]
                 ],
                 [
-                    'node_id'   => '9',
+                    'node_id'   => '10',
                     'node_name' => 'cashier',
-                    'title'     => '前台管理',
+                    'title'     => '前台账号',
                     'child'    => [
                         [
-                            'node_id'   => '10',
+                            'node_id'   => '11',
                             'node_name' => 'getCashier',
                             'title'     => '收银员查询',
                             'resource'  => ['/employee/getEmployeeList']
                         ],
                         [
-                            'node_id'   => '11',
+                            'node_id'   => '12',
                             'node_name' => 'addEmployee',
                             'title'     => '新增/更新/删除前台',
                             'resource'  => ['/employee/addCashier', '/employee/updateCashier', '/employee/removeCashier']
@@ -130,70 +130,143 @@ return [
             ]
         ],
         [
-            'node_name' => 'authManage',
-            'title'     => '权限管理',
+            'node_name' => 'user-center',
+            'title'     => '会员中心',
             'node_id'   => '20',
             'child'    => [
                 [
                     'node_id'   => '21',
-                    'node_name' => 'accountManage',
-                    'title'     => '帐号管理',
-                    'child'    => [
+                    'node_name' => 'user-mgr',
+                    'title'     => '会员管理',
+                    'child'     => [
                         [
                             'node_id'   => '22',
-                            'node_name' => 'accountList',
-                            'title'     => '账户列表',
-                            'resource'  => ['/admin/accountList']
+                            'node_name' => 'getUsers',
+                            'title'     => '会员/详情查询',
+                            'resource'  => ['/users/getUserList','/users/getUserDetail','/users/getUserItemList', '/users/getOrderList', '/users/getUseOrderList']
                         ],
                         [
                             'node_id'   => '23',
-                            'node_name' => 'modifyAccountPasswd',
-                            'title'     => '修改密码',
-                            'resource'  => ['/admin/modifyAccountPasswd']
+                            'node_name' => 'addUser',
+                            'title'     => '创建/编辑用户',
+                            'resource'  => ['/users/addUser', '/users/updateUser']
                         ],
                         [
                             'node_id'   => '24',
-                            'node_name' => 'modifyRole',
-                            'title'     => '更改角色',
-                            'resource'  => ['/admin/modifyRole']
-                        ],
+                            'node_name' => 'importUser',
+                            'title'     => '导入用户',
+                            'resource'  => ['/users/importUser']
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        [
+            'node_name' => 'orders',
+            'title'     => '收银单据',
+            'node_id'   => '25',
+            'child'    => [
+                [
+                    'node_id'   => '26',
+                    'node_name' => 'yeji-order',
+                    'title'     => '业绩单据',
+                    'child'     => [
                         [
-                            'node_id'   => '25',
-                            'node_name' => 'delAccount',
-                            'title'     => '删除帐号',
-                            'resource'  => ['/admin/disableAccount']
-                        ],
-                        [
-                            'node_id'   => '26',
-                            'node_name' => 'openAccount',
-                            'title'     => '创建帐号',
-                            'resource'  => ['/admin/openAccount', '/admin/getRoleList']
+                            'node_id'   => '27',
+                            'node_name' => 'yeji-order-list',
+                            'title'     => '业绩单据查询',
+                            'resource'  => ['/order/getOrderList']
                         ]
                     ]
                 ],
                 [
                     'node_id'   => '28',
+                    'node_name' => 'xiaohao-order',
+                    'title'     => '消耗单据',
+                    'child'     => [
+                        [
+                            'node_id'   => '29',
+                            'node_name' => 'xiaohao-order-list',
+                            'title'     => '消耗单据查询',
+                            'resource'  => ['/order/getUseOrderList']
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        [
+            'node_name' => 'data-analysis',
+            'title'     => '数据统计',
+            'node_id'   => '30',
+            'child'    => [
+                [
+                    'node_id'   => '31',
+                    'node_name' => 'shop-data',
+                    'title'     => '门店数据',
+                    'child'     => [
+                        [
+                            'node_id'   => '32',
+                            'node_name' => 'getUsers',
+                            'title'     => '门店数据',
+                            'resource'  => ['/storeData/getShopsDataView']
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        [
+            'node_name' => 'authManage',
+            'title'     => '权限管理',
+            'node_id'   => '1001',
+            'child'    => [
+                [
+                    'node_id'   => '1002',
                     'node_name' => 'roleManage',
                     'title'     => '角色管理',
                     'child'    => [
                         [
-                            'node_id'   => '29',
+                            'node_id'   => '1003',
                             'node_name' => 'addRole',
                             'title'     => '创建角色',
                             'resource'  => ['/admin/addRole']
                         ],
                         [
-                            'node_id'   => '30',
+                            'node_id'   => '1004',
                             'node_name' => 'modifyRoleAccess',
                             'title'     => '修改权限',
                             'resource'  => ['/admin/modifyRoleAccess', '/admin/roleAccess']
                         ],
                         [
-                            'node_id'   => '31',
+                            'node_id'   => '1005',
                             'node_name' => 'disableRole',
                             'title'     => '删除角色',
                             'resource'  => ['/admin/disableRole']
                         ],
+                    ]
+                ],
+                [
+                    'node_id'   => '1006',
+                    'node_name' => 'accountManage',
+                    'title'     => '帐号管理',
+                    'child'    => [
+                        [
+                            'node_id'   => '1007',
+                            'node_name' => 'accountList',
+                            'title'     => '账户列表',
+                            'resource'  => ['/admin/getAccountList']
+                        ],
+                        [
+                            'node_id'   => '1008',
+                            'node_name' => 'delAccount',
+                            'title'     => '删除帐号',
+                            'resource'  => ['/admin/disableAccount']
+                        ],
+                        [
+                            'node_id'   => '1009',
+                            'node_name' => 'openAccount',
+                            'title'     => '创建帐号',
+                            'resource'  => ['/admin/openAccount', '/admin/getRoleList']
+                        ]
                     ]
                 ]
             ]
--- routes/admin.php
+++ routes/admin.php
@@ -34,12 +34,12 @@ Route::group(['middleware' => ['AdminloginState']], function () {
     Route::post('/employee/importEmployee', 'Employee\EmployeeController@importEmployee');  //导入员工信息
     Route::post('/employee/removeEmployee', 'Employee\EmployeeController@removeEmployee');  //删除员工
     Route::post('/employee/getEmployeeInfo', 'Employee\EmployeeController@getEmployeeInfo');  //获取员工信息
+    Route::post('/employee/getEmpDataView', 'DataViews\EmpDataController@getEmpDataView');  //获取用户数据接口
+    Route::post('/employee/getEmpOrderList', 'Employee\EmployeeOrderController@getEmpOrderList');  //获取用户数据接口
 
     Route::post('/employee/addCashier', 'Employee\CashierController@addCashier');  // 增加收银账号
     Route::post('/employee/updateCashier', 'Employee\CashierController@updateCashier');  //更新收银员信息
     Route::post('/employee/removeCashier', 'Employee\CashierController@removeCashier');  //删除收银员
-
-
 
     Route::post('/item/addItemType', 'Item\ItemController@addItemType');  //增加疗程类别
     Route::post('/item/getItemType', 'Item\ItemController@getItemType');  //获取疗程项目类别
@@ -47,7 +47,6 @@ Route::group(['middleware' => ['AdminloginState']], function () {
     Route::post('/item/addItem', 'Item\ItemController@addItem');  //增加项目
     Route::post('/item/getItemList', 'Item\ItemController@getItemList'); //增加项目列表
     Route::post('/item/modifyItem', 'Item\ItemController@modifyItem'); //修改项目
-
 
     Route::post('/goods/addGoodBrand', 'Goods\GoodBrandController@addGoodBrand'); //增加商品品牌
     Route::post('/goods/getBrandList', 'Goods\GoodBrandController@getBrandList'); //获取品牌列表
@@ -59,13 +58,11 @@ Route::group(['middleware' => ['AdminloginState']], function () {
 
     Route::post('/users/getUserList', 'Users\UsersController@getUserList');  //获取会员列表
     Route::post('/users/updateUser', 'Users\UsersController@updateUser');  //更新会员信息
-
     Route::post('/users/getUserDetail', 'Users\UsersController@getUserDetail');  //获取用户详情
     Route::post('/users/getUserItemList', 'Users\UsersAccountController@getItemList');  //获取用户的服务数据
     Route::post('/users/getOrderList', 'Users\UsersAccountController@getOrderList');  //获取用户购买记录
     Route::post('/users/getUseOrderList', 'Users\UsersAccountController@getUseOrderList');  //耗卡记录
     Route::post('/users/addUser', 'Users\UsersController@addUser');  //创建用户
-
     Route::post('/users/importUser', 'Users\UsersController@importUser');  //导入用户
 
     Route::post('/order/getUseOrderList', 'Users\UsersAccountController@getUseOrderList');  //获取消耗记录
@@ -74,26 +71,18 @@ Route::group(['middleware' => ['AdminloginState']], function () {
     Route::post('/admin/resetPassword', 'Auth\ResetPasswordController@resetAdminPassword');  //修改密码接口
 
 
-    Route::post('/employee/getEmpDataView', 'DataViews\EmpDataController@getEmpDataView');  //获取用户数据接口
-    Route::post('/employee/getEmpOrderList', 'Employee\EmployeeOrderController@getEmpOrderList');  //获取用户数据接口
-
-
     Route::post('/storeData/getShopsDataView', 'DataViews\ShopDataController@getShopsDataView');  //获取门店总数据
 
     Route::post('/admin/addRole', 'Auth\AdminAuthController@addRole');  //添加角色
     Route::post('/admin/getRoleList', 'Auth\AdminAuthController@getRoleList');  //获取角色列表
-    Route::post('/admin/getNode', 'Auth\AdminAuthController@getNode');  //获取权限节点
     Route::post('/admin/roleAccess', 'Auth\AdminAuthController@roleAccess');  //获取权限节点
     Route::post('/admin/modifyRoleAccess', 'Auth\AdminAuthController@modifyRoleAccess');  //修改权限
     Route::post('/admin/disableRole', 'Auth\AdminAuthController@disableRole');  //删除角色
+
+    Route::post('/admin/getNode', 'Auth\AdminAuthController@getNode');  //获取权限节点
     Route::post('/admin/openAccount', 'Auth\AdminAuthController@openAccount');  //创建管理员账号
     Route::post('/admin/getAccountList', 'Auth\AdminAuthController@getAccountList');  //创建管理员账号
     Route::post('/admin/disableAccount', 'Auth\AdminAuthController@disableAccount');  //删除账号
-
-
-
-
-
 
 
 });
